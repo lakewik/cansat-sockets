89694933e2b8e92c52c275257a816125f308f416
Wiktor Jezioro <wikjezioro@op.pl>
2018-06-04 21:07:05 +0200

    Add postgres client

--- ws-postgresql-inserter/pgsql_client.js
+++ ws-postgresql-inserter/pgsql_client.js
@@ -1,1 +1,24 @@
+const { Pool, Client } = require('pg')
+const WebSocket = require('ws');
 
+const ws = new WebSocket('ws://localhost:7777');
+ 
+ws.on('open', function open() {
+});
+
+const pool = new Pool({
+  user: 'postgres',
+  host: 'locslhost',
+  database: 'cansat',
+  password: 'kotek',
+  port: 3211,
+})
+
+ 
+ws.on('message', function incoming(data) {
+  console.log(data);
+  pool.query('INSERT INTO', (err, res) => {
+    console.log(err, res)
+    pool.end()
+  })
+});
