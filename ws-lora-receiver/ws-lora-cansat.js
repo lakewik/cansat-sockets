731006210d5ea205f23aa483cc508f271d51dc59
Wiktor Jezioro <lakewik@lakewik.pl>
2018-06-15 21:14:44 +0200

    Add RSSI to LoRA receiver

--- ws-lora-receiver/ws-lora-cansat.js
+++ ws-lora-receiver/ws-lora-cansat.js
@@ -27,7 +27,7 @@ sx127x.open(function(err) {
   // add a event listener for data events
   sx127x.on('data', function(data, rssi) {
     console.log('data:', '\'' + data.toString() + '\'', rssi);
-    ws.send(data.toString());
+    ws.send(data.toString()+rssi.toString()+"|");
   });
 
   // enable receive mode
